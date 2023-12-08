90705799b7d086b8035e2486edb8581a44682fc5
SergioCR3 <148821425+SergioCR3@users.noreply.github.com>
2023-12-08 20:57:24 +0100

    08/12

--- index.php
+++ index.php
@@ -8,55 +8,85 @@
         .popup-content {
             max-width: 500px;
         }
+        img{
+            max-height: 80px;
+        }
         html, body {
             height: 100%;
             margin: 0;
             padding: 0;
         }
         #map {
-            height: 100vh;
+            height: 80vh;
+        }
+        #buttons {
+            margin-top: 10px;
+        }
+        button {
+            margin-right: 10px;
         }
     </style>
 </head>
 <body>
     <div id="map"></div>
+    <div id="buttons">
+        <button onclick="filterMarkers('Camariñas')">Camariñas</button>
+        <button onclick="filterMarkers('Vimianzo')">Vimianzo</button>
+        <button onclick="filterMarkers('Zas')">Zas</button>
+        <button onclick="showAllMarkers()">Mostrar Todos</button>
+    </div>
 
     <script>
-        function agregarMarcador(lat, lng, name, description) {
-            var marker = L.marker([lat, lng]).addTo(map);
-            marker.bindPopup('<b>' + name + '</b><br>' + description);
-        }
-
-        var map = L.map('map').setView([43.102324, -9.0707226], 12); // Cambia las coordenadas al centro de Terra de Soneira.
+        var map = L.map('map').setView([43.102324, -9.0707226], 12);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
         }).addTo(map);
 
+        var geojsonLayer;
+
+        function agregarMarcador(lat, lng, name, description, image) {
+            var marker = L.marker([lat, lng]).addTo(map);
+            marker.bindPopup('<div class="popup-content"><b>' + name + '</b><br>' + description + '<br><img src="' + image + '" alt=" "></div>');
+        }
+
+        function filterMarkers(municipality) {
+            map.eachLayer(function (layer) {
+                if (layer instanceof L.Marker) {
+                    map.removeLayer(layer);
+                }
+            });
+
+            geojsonLayer.eachLayer(function (layer) {
+                var properties = layer.feature.properties;
+                if (properties.municipality === municipality) {
+                    agregarMarcador(layer._latlng.lat, layer._latlng.lng, properties.name, properties.description, properties.image);
+                }
+            });
+        }
+
+        function showAllMarkers() {
+            map.eachLayer(function (layer) {
+                if (layer instanceof L.Marker) {
+                    map.removeLayer(layer);
+                }
+            });
+
+            geojsonLayer.eachLayer(function (layer) {
+                var properties = layer.feature.properties;
+                agregarMarcador(layer._latlng.lat, layer._latlng.lng, properties.name, properties.description, properties.image);
+            });
+        }
+
         // Cargar datos de GeoJSON desde un archivo
-fetch('lugares_interes.geojson')
-    .then(response => response.json())
-    .then(data => {
-        L.geoJSON(data, {
-            pointToLayer: function (feature, latlng) {
-                return L.marker(latlng).on('click', function () {
-                    let popupContent = '<b>No data</b>'; // Mensaje predeterminado
-
-                    if (feature.properties && feature.properties.name) {
-                        popupContent = '<b>' + feature.properties.name + '</b>';
-                    }
-
-                    if (feature.properties && feature.properties.description) {
-                        popupContent += '<br>' + feature.properties.description;
-                    }
-
-                    this.bindPopup(popupContent).openPopup();
-                });
-            }
-        }).addTo(map);
-    })
-    .catch(error => {
-        console.error('Error loading GeoJSON:', error);
-    });    </script>
+        fetch('lugares_interes.geojson')
+            .then(response => response.json())
+            .then(data => {
+                geojsonLayer = L.geoJSON(data).addTo(map);
+            })
+            .catch(error => {
+                console.error('Error loading GeoJSON:', error);
+            });
+    </script>
 </body>
 </html>
